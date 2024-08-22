b1b316d6341bcadcf53ae49d682eb01ff728c267
334688 <a334688@MXGDLATLW96C2MR.local>
2024-08-22 12:48:08 -0600

    Fix versions sintax

--- versions.tf
+++ versions.tf
@@ -14,7 +14,9 @@ terraform {
   required_version = ">= 1.5.0, <= 1.5.5"
 
   required_providers {
-    aws    = ">= 3.57.0"
-    random = ">= 3.4.3"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
